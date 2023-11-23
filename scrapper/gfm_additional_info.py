54e864ccdc8247bf8193c2573b76942cf861098b
Lue12138 <shawlue2@gmail.com>
2023-11-23 18:22:23 -0500

    update the code to write data immediately

--- scrapper/gfm_additional_info.py
+++ scrapper/gfm_additional_info.py
@@ -26,18 +26,10 @@ def extract_date(url):
 
 
 def main(start_row, end_row):
-    urls = []
-
     with open('./sample.csv', 'r', newline='', encoding='utf-8') as urlfile:
         url_reader = csv.reader(urlfile)
         rows = list(url_reader)
         header = rows[0]
-
-        for idx, row in enumerate(rows[1:], start=1):
-            if start_row <= idx <= end_row:
-                urls.append(row[1])  # Change index to 1 to get the URL from the second column
-            elif idx > end_row:
-                break
 
     count = 0
     max_requests_before_delay = 500
@@ -45,41 +37,41 @@ def main(start_row, end_row):
 
     updated_rows = [header + ['length']]
 
-    for idx, url in enumerate(urls):
-        print(f"Extracting specific element from: {url}")
-        specific_element_info = extract_date(url)
-        if all(element is not None for element in specific_element_info):
-            now = "Nov 6, 2023"  # the date that we scrape the data
-            date_raw = specific_element_info[1].split()
-            day = date_raw[1][:-3]
-            # print(day)
+    for idx, row in enumerate(rows[1:], start=1):
+        if start_row <= idx <= end_row:
+            url = row[1]  # Change index to 1 to get the URL from the second column
 
-            # Convert the date strings to datetime objects
-            now = datetime.strptime(now, "%b %d, %Y")
-            created_date = datetime(int(date_raw[2]), datetime.strptime(date_raw[0], "%B").month, int(day))
+            print(f"Extracting specific element from: {url}")
+            specific_element_info = extract_date(url)
+            if all(element is not None for element in specific_element_info):
+                now = "Nov 6, 2023"  # the date that we scrape the data
+                date_raw = specific_element_info[1].split()
+                day = date_raw[1][:-3]
 
-            time_difference = now - created_date
+                # Convert the date strings to datetime objects
+                now = datetime.strptime(now, "%b %d, %Y")
+                created_date = datetime(int(date_raw[2]), datetime.strptime(date_raw[0], "%B").month, int(day))
 
-            # Extract the number of days from the timedelta object
-            days_difference = time_difference.days
+                time_difference = now - created_date
 
-            # Add the length as a new column
-            row = rows[idx + 1].copy()
-            row.append(days_difference)
-            updated_rows.append(row)
+                # Extract the number of days from the timedelta object
+                days_difference = time_difference.days
 
-        count += 1
-        if count % max_requests_before_delay == 0:
-            print(f"Waiting for {delay_seconds} seconds to avoid being detected as a bot...")
-            time.sleep(delay_seconds)
+                # Add the length as a new column
+                row.append(days_difference)
+                updated_rows.append(row)
 
-    # Write the updated rows to a new file
-    new_file_path = f'./scrapper/data/campaign_info_updated_{start_row}_{end_row}.csv'
-    with open(new_file_path, 'w', newline='', encoding='utf-8') as csvfile:
-        url_writer = csv.writer(csvfile)
-        url_writer.writerows(updated_rows)
+                # Write the updated row to the file immediately
+                with open(f'./scrapper/data/campaign_info_updated_{start_row}_{end_row}.csv', 'a', newline='', encoding='utf-8') as csvfile:
+                    url_writer = csv.writer(csvfile)
+                    url_writer.writerow(row)
 
-    print(f"Updated rows written to {new_file_path}")
+            count += 1
+            if count % max_requests_before_delay == 0:
+                print(f"Waiting for {delay_seconds} seconds to avoid being detected as a bot...")
+                time.sleep(delay_seconds)
+
+    print(f"Updated rows written to ./scrapper/data/campaign_info_updated_{start_row}_{end_row}.csv")
 
 
 if __name__ == "__main__":
